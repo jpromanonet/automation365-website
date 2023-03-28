50e0bd9c7e20d1a4bae5544632753fe70bc5e6d2
jpromanonet <programming@jpromano.net>
2023-03-28 18:28:08 -0300

    Layout

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 export default function Header() {
   return (
     <Head>
-      <title>Jananadi W</title>
+      <title>Automating Something Everyday</title>
       <meta name="description" content="Developer portfolio site" />
       <meta
         name="image"
